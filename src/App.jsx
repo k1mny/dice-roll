5e11b26f570fa2a3da147c8108acbf47c97c5195
KKimura <19808712+k1mny@users.noreply.github.com>
2022-03-18 21:37:37 +0900

    fix for mobile height

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Dice from './components/dice';
 
 function App() {
+  // for mobile height
+  useEffect(() => {
+    const setFillHeight = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+    window.addEventListener('resize', setFillHeight);
+    setFillHeight();
+  }, []);
+
   return (
     <div className="App">
       <Dice />
